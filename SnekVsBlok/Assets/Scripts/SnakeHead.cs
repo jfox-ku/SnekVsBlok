bb39d3705fbc654cc5d6b30ac94437173da8edaf
Atahan Yılmaz <atahanylmaz@gmail.com>
2023-08-22 22:36:20 +0300

    Snake path

--- SnekVsBlok/Assets/Scripts/SnakeHead.cs
+++ SnekVsBlok/Assets/Scripts/SnakeHead.cs
@@ -2,16 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SnakeHead : MonoBehaviour
 {
+    [SerializeField] private SnakePath _snakePath = new SnakePath();
+    
     [SerializeField] private bool _isStart;
     [SerializeField] private bool _isFirstTouch;
     [SerializeField] private Vector2 _lastTouchPosition;
     [SerializeField] private Vector2 _velocity;
     [SerializeField] private Vector2 _bounds = new Vector2(-6, 6);
+    [SerializeField] private float _cameraForwardOffset = 3f;
     [SerializeField] private List<Transform> _snakeBody;
-    [SerializeField] private float _distanceBetweenParts = 1f;
+    //[SerializeField] private float _distanceBetweenParts = 1f;
     [SerializeField] private float _horizontalVelocity = 5f;
     [SerializeField] private float _verticalVelocity = 1f;
 
@@ -22,17 +26,30 @@ public class SnakeHead : MonoBehaviour
 
     private void Start()
     {
+        Application.targetFrameRate = 60;
         _camera = Camera.main.transform;
         _velocity = Vector2.zero;
         _isStart = false;
         UpdateCamera();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
     private void FixedUpdate()
     {
         var isTouch = Input.GetMouseButton(0);
-        
-        if (!_isStart && isTouch) _isStart = true;
+
+        if (!_isStart && isTouch)
+        {
+            UpdateLastTouchPosition();
+            _isStart = true;
+        }
         
         if (!_isStart) return;
         
@@ -51,6 +68,7 @@ public class SnakeHead : MonoBehaviour
         
         if (isTouch && !_isFirstTouch) UpdateLastTouchPosition();
 
+        
         UpdateHeadPosition();
         UpdateCamera();
         UpdateTail();
@@ -65,22 +83,29 @@ public class SnakeHead : MonoBehaviour
     {
         var newPos = transform.position + (Vector3) _velocity;
         newPos[0] = Mathf.Clamp(newPos[0], _bounds.x, _bounds.y);
-        transform.position = newPos;
+        //transform.position = newPos;
+        _snakePath.SnakeLength = _snakeBody.Count;
+        _snakePath.AddHeadPosition(newPos);
     }
 
     private void UpdateCamera()
     {
         var cameraPos = _camera.position;
-        cameraPos[1] = transform.position.y;
+        cameraPos[1] = transform.position.y + _cameraForwardOffset;
         _camera.position = cameraPos;
     }
 
     private void UpdateTail()
     {
-        for (int i = 0; i < _snakeBody.Count; i++)
+        for (var i = 0; i < _snakeBody.Count; i++)
         {
             var part = _snakeBody[i];
-            var target = i == 0 ? transform : _snakeBody[i - 1];
+            part.position = _snakePath[i];
+            // var target = (i == 0) ? transform : _snakeBody[i - 1];
+            // var targetPos = target.position;
+            // var dir = targetPos - part.position;
+            // var newDir = Vector3.ClampMagnitude(dir,_distanceBetweenParts);
+            // part.position = targetPos - newDir;
         }
     }
 }
---
+++ SnekVsBlok/Assets/Scripts/SnakePath.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SnakePath
+{
+    [SerializeField]
+    private float _maxDistanceBetweenNodes = 1f;
+    
+    [SerializeField]
+    public int SnakeLength { get; set; }
+
+    private List<Vector2> _headPositions = new List<Vector2>();
+    private Dictionary<int,Vector2> _nodePositions = new Dictionary<int, Vector2>();
+
+    public Vector2 this[int index]
+    {
+        get
+        {
+            if (_nodePositions.ContainsKey(index))
+            {
+                return _nodePositions[index];
+            }
+            
+            return _headPositions[0];
+        }
+    }
+
+    public void AddHeadPosition(Vector2 pos)
+    {
+        _headPositions.Insert(0,pos);
+        UpdateNodePositions();
+    }
+
+    public void ClearOldPositions(int lastUsedIndex)
+    {
+        for (var i = _headPositions.Count - 1; i > lastUsedIndex; i--)
+        {
+            _headPositions.RemoveAt(i);
+        }
+        
+        Debug.Log("head Positions count: " + _headPositions.Count); 
+    }
+
+    public void UpdateNodePositions()
+    {
+        int LastUpdatedNode = 0;
+        int LastUsedHeadIndex = 0;
+        if (_headPositions == null || _headPositions.Count == 0) return;
+        Vector2 LatestNodePos = _headPositions[0];
+        float distanceAddUp = 0;
+        
+        float DistanceToLatestNode(Vector2 currentPos) =>  VectorToLatestNode(currentPos).magnitude;
+        Vector2 VectorToLatestNode(Vector2 currentPos) =>  (currentPos - LatestNodePos);
+        
+        for (int i = 0; i < _headPositions.Count; i++)
+        {
+            if (i == 0)
+            {
+                _nodePositions[0] = _headPositions[0];
+                continue;
+            }
+
+            LastUsedHeadIndex = i;
+            if (LastUpdatedNode >= SnakeLength) break;
+
+            distanceAddUp += (_headPositions[i]-_headPositions[i-1]).magnitude;
+            if (!(distanceAddUp >= _maxDistanceBetweenNodes)) continue;
+            
+            var vectorToLatestNode = VectorToLatestNode(_headPositions[i]);
+            var clamped = Vector2.ClampMagnitude(vectorToLatestNode,_maxDistanceBetweenNodes);
+            var clampedPos = LatestNodePos + clamped;
+            _nodePositions[++LastUpdatedNode] = clampedPos;
+            LatestNodePos = clampedPos;
+            distanceAddUp -= _maxDistanceBetweenNodes;
+            //distanceAddUp = 0;
+
+        }
+        
+        ClearOldPositions(LastUsedHeadIndex);
+    }
+}
